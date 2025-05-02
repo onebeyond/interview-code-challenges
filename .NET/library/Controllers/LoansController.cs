75cd1c653c804a7040469b674fca39af5b3b0d1e
Matthew Fisk <matthew.fisk@greenlordstudios.co.uk>
2025-05-02 11:44:59 +0100

    Created a loan controller and crafted an endpoint to get all books currently on loan and who they are on loan to

---
+++ .NET/library/Controllers/LoansController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneBeyondApi.DataAccess;
+using OneBeyondApi.Model;
+
+namespace OneBeyondApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
+    {
+        private readonly ILogger<LoanController> _logger;
+        private readonly ICatalogueRepository _catalogueRepository;
+        private readonly IBorrowerRepository _borrowerRepository;
+
+        public LoanController(ILogger<LoanController> logger, ICatalogueRepository catalogueRepository, IBorrowerRepository borrowerRepository)
+        {
+            _logger = logger;
+            _catalogueRepository = catalogueRepository;
+            _borrowerRepository = borrowerRepository;
+        }
+
+        [HttpGet]
+        [Route("GetOnLoan")]
+        public IActionResult GetOnLoan()
+        {
+            var onLoan = _catalogueRepository
+                .GetCatalogue()
+                .Where(stock => stock.OnLoanTo != null)
+                .Select(stock => new
+                {
+                    Borrower = stock.OnLoanTo.Name,
+                    Email = stock.OnLoanTo.EmailAddress,
+                    BookTitle = stock.Book.Name,
+                    DueDate = stock.LoanEndDate
+                })
+                .ToList();
+
+            return Ok(onLoan);
+        }
+    }
+}
