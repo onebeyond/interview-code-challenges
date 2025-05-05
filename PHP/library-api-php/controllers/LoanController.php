3da67432d01b4b5143ab23b3ade1b60bbbe4dc85
Balazs Skerlecz <bskerlecz@gmail.com>
2025-05-05 22:13:56 +0200

    PHP engineer challenge

--- PHP/library-api-php/controllers/LoanController.php
+++ PHP/library-api-php/controllers/LoanController.php
@@ -7,15 +7,15 @@ require_once __DIR__ . '/../models/Fine.php';
 class LoanController {
     // GET /loans
     public function index() {
-        // TODO: Implement logic to list active loans with borrower and book details.
         header('Content-Type: application/json');
-        echo json_encode(['message' => 'List active loans functionality to be implemented.']);
+        header('Status: 200');
+        echo json_encode(bookList());
     }
     
     // POST /loans/return
     public function returnBook() {
-        // TODO: Implement logic to process the return of a book and calculate fines if overdue.
         header('Content-Type: application/json');
-        echo json_encode(['message' => 'Return book functionality to be implemented.']);
+        header('Status: 200');
+        echo json_encode(returnBook($_POST['bookId']));
     }
 }
